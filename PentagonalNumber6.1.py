6f955da9654dcfd3b9e05ae2ed02a4d77d5f4daf
atwade69 <35306316+atwade69@users.noreply.github.com>
2018-06-23 22:36:25 -0400

    Putting function on Git

--- PentagonalNumber6.1.py
+++ PentagonalNumber6.1.py
@@ -1,9 +1,10 @@
+#function to find pentagonal number of value N
 def getPentagonalNumber(n):
     solution = n*(3*n-1)/2
     return solution
 
 
 for i in range(101):
-    print(int(getPentagonalNumber(i)),end=" ")
+    print(int(getPentagonalNumber(i)),end="")
     if i%10==0:
         print()
