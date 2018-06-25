71c5b378a97747c5cf5391875d899add6a3715fd
atwade69 <35306316+atwade69@users.noreply.github.com>
2018-06-25 13:49:47 -0400

    New name and added comments

--- Timesincespoch.py
+++ Timesincespoch.py
@@ -1,12 +1,14 @@
+# Time Functions , shows in UTC time which is 4 hours difference than est
+
 import time
 
-#test if leap year
+# Test if leap year returns true or false. If divisible by 400 or divisible by 4 but not 100 makes it a leap year
 def leap(year):
-    return year%400==0 or(year%4==0 and year%100!=0)
+    return year%400 == 0 or(year%4 == 0 and year%100 != 0)
 
-#many many days are in each month, once you get to the year you can tell the date by the number
+# Many many days are in each month, once you get to the year you can tell the date by the number
 def month(days,year):
-    #test if the year is a leap year
+    # Test if the year is a leap year then supplies what day it would be based on days in a leap year
     if leap(year):
         if day<=31:
             month = "January"
@@ -32,6 +34,7 @@ def month(days,year):
             month = "Novemeber"
         elif days <=366:
             month = "December"
+    # If not leap year then it is has one less day in February
     else:
         if days <= 31:
             month = "January"
@@ -59,7 +62,7 @@ def month(days,year):
             month = "December"
     return month
 
-
+# Test what day it is based on days in a year
 def date(days,year):
     days = days
     if leap(year):
@@ -114,6 +117,7 @@ def date(days,year):
             days =days-334
     return days
 
+#Makes clock show hour in am or pm
 def amPmTime(hour):
     if hour>12:
         hour-=12
@@ -153,15 +157,15 @@ def dateTime():
 
 
 
-
-    if currentHour>=12 and currentHour<24:
+# Formatting for display of time
+    if currentHour >=12  and currentHour < 24:
 
         print(amPmTime(currentHour),":",currentMinute,":",currentSecond," PM ",month(totalDays,year)," ",date(totalDays,year),", ",year,sep="")
 
-
+# Formatting for display of time
     else:
-        if currentHour==0:
-            currentHour=12
+        if currentHour == 0:
+            currentHour = 12
         print(amPmTime(currentHour), ":", currentMinute, ":", currentSecond, " AM ", month(totalDays, year), " ",
               date(totalDays, year), ", ", year, sep="")
 
