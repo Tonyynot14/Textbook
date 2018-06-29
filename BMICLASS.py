39c885936cc1349fd481bd3d94323440823b5024
atwade69 <35306316+atwade69@users.noreply.github.com>
2018-06-29 12:15:22 -0400

    BMI class and class test

---
+++ BMICLASS.py
@@ -0,0 +1,31 @@
+class BMI:
+    def __init__(self,name,age,weight,height):
+        self.__name = name
+        self.__age = age
+        self.__weight = weight
+        self.__height = height
+
+    def getName(self):
+        return self.__name
+    def getAge(self):
+        return self.__age
+    def getWeight(self):
+        return self.__weight
+    def getHeight(self):
+        return self.__height
+    def getBMI(self):
+        KILOGRAMS_PER_POUND = .453559237
+        METERS_PER_INCH = .0254
+        bmi = self.__weight*KILOGRAMS_PER_POUND/((self.__height*METERS_PER_INCH)**2)
+        return round(bmi*100)/100
+    def getStatus(self):
+        bmi = self.getBMI()
+        if bmi < 18.5:
+            return "Under Weight"
+        elif bmi < 25:
+            return "Normal"
+        elif bmi < 30:
+            return "Over weight"
+        else:
+            return "Obese"
+
