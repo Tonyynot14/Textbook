6d48bcf10c4bb26603b54f8f93190a3997c8cd37
atwade69 <35306316+atwade69@users.noreply.github.com>
2018-07-06 15:12:04 -0400

    stopWatch comments

--- stopwatchtest.py
+++ stopwatchtest.py
@@ -1,11 +1,15 @@
 from stopwatch import StopWatch
 
+# creation of stopwatch class object
 clock = StopWatch()
 
+# use of clock method start
 clock.start()
 sum = 0
+# Random loop to test how long it takes computer to complete
 for i in range(1,1000001):
     sum+=i
+# use of clock method stop
 clock.stop()
-
+# use of clock method getElapsedTime to display how long between the two events
 print("The amount of time for the calculation to run is",clock.getElapsedTime(), "milliseconds")
